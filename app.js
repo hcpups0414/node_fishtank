be7811961c5021a92b1dc591b22dff52bdf1cc22
tony <hcpups0414@gmail.com>
2017-11-24 10:56:04 +0800

    change

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -36,22 +35,23 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json('error', {
+    res.status(err.status || 500)
+    .json({
       message: err.message,
       error: err
-    });
+    });    
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json('error', {
+  res.status(err.status || 500)
+  .json({
     message: err.message,
     error: {}
   });
+  
 });
 
 
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    host: '192.168.1.114',
+    port: '3000'
+}
